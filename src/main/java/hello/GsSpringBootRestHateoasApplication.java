8a49f95c2d6f030d0f33b021c79dd2853ce8da84
Renato Martins <renatto.martins@gmail.com>
2017-07-06 12:09:30 -0300

    Adds the spring boot starter project files

---
+++ src/main/java/hello/GsSpringBootRestHateoasApplication.java
@@ -0,0 +1,12 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsSpringBootRestHateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsSpringBootRestHateoasApplication.class, args);
+	}
+}
